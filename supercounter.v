d71371451bac0e24ffc62f63d9af716391116b6b
Carl Rivera <riverac3@IITS-I402-05.utsc-labs.utoronto.ca>
2018-03-27 17:38:03 -0400

    $

--- supercounter.v
+++ supercounter.v
@@ -69,3 +69,87 @@ module pulse_1500mill(clock, reset, enable, pulse, q, sub);
 	
 	assign pulse =  (q == 31'b0000000_00000000_00000000_00000000) ? 1 : 0;
 endmodule
+
+module pulse_100mill(clock, reset, enable, pulse, q);
+	input clock;
+	input reset;
+	input enable;
+	output pulse;
+	
+	output reg [26:0] q; // declare q
+	
+	always @(posedge clock) // triggered every time clock rises
+	begin
+		if (reset == 1'b1) // when reset is 1
+			q <= 27'b101_11110101_11100000_11111111;
+		else if (q == 27'b000_00000000_00000000_00000000) //maybe dont reset it like this?
+			q <= 27'b101_11110101_11100000_11111111;
+		else if (enable == 1'b1)
+			q <= q - 1'b1; // decrement q
+	end
+	
+	assign pulse =  (q == 27'b000_00000000_00000000_00000000) ? 1 : 0;
+endmodule
+
+module pulse_50mill(clock, reset, enable, pulse, q);
+	input clock;
+	input reset;
+	input enable;
+	output pulse;
+	
+	output reg [25:0] q; // declare q
+	
+	always @(posedge clock) // triggered every time clock rises
+	begin
+		if (reset == 1'b1) // when reset is 1
+			q <= 26'b10_11111010_11110000_01111111;
+		else if (q == 26'b00_00000000_00000000_00000000) //maybe dont reset it like this?
+			q <= 26'b10_11111010_11110000_01111111;
+		else if (enable == 1'b1)
+			q <= q - 1'b1; // decrement q
+	end
+	
+	assign pulse =  (q == 26'b00_00000000_00000000_00000000) ? 1 : 0;
+endmodule
+
+module pulse_25mill(clock, reset, enable, pulse, q);
+	input clock;
+	input reset;
+	input enable;
+	output pulse;
+	
+	output reg [24:0] q; // declare q
+	
+	always @(posedge clock) // triggered every time clock rises
+	begin
+		if (reset == 1'b1) // when reset is 1
+			q <= 25'b1_01111101_01111000_00111111;
+		else if (q == 25'b0_00000000_00000000_00000000) //maybe dont reset it like this?
+			q <= 25'b1_01111101_01111000_00111111;
+		else if (enable == 1'b1)
+			q <= q - 1'b1; // decrement q
+	end
+	
+	assign pulse =  (q == 25'b0_00000000_00000000_00000000) ? 1 : 0;
+endmodule
+
+module pulse_10mill(clock, reset, enable, pulse, q);
+	input clock;
+	input reset;
+	input enable;
+	output pulse;
+	
+	output reg [23:0] q; // declare q
+	
+	always @(posedge clock) // triggered every time clock rises
+	begin
+		if (reset == 1'b1) // when reset is 1
+			q <= 24'b10011000_10010110_01111111;
+		else if (q == 24'b00000000_00000000_00000000) //maybe dont reset it like this?
+			q <= 24'b10011000_10010110_01111111;
+		else if (enable == 1'b1)
+			q <= q - 1'b1; // decrement q
+	end
+	
+	assign pulse =  (q == 24'b00000000_00000000_00000000) ? 1 : 0;
+endmodule
